2619c9dd7174f33ae14ff069677c48ef7af0341f
Andreea <muntean.anda2000@gmail.com>
2021-03-04 12:36:48 +0200

    chore: fix stash conflicts

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import Paper from '@material-ui/core/Paper'
 import UserView from './tablet/UserView'
 
@@ -6,6 +7,20 @@ function Root() {
         <Paper>
             <UserView />
         </Paper>
+=======
+import Login from './login/Login'
+import UserView from './userView/UserView'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+
+function Root() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/userView" component={UserView} />
+            </Switch>
+        </BrowserRouter>
+>>>>>>> Stashed changes
     )
 }
 
