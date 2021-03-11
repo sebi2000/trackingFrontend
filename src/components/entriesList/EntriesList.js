286455d2be5f2bbca02cd18e127a1dcac658a40d
Andreea <muntean.anda2000@gmail.com>
2021-03-11 10:00:06 +0200

    feat: create entries table

--- src/components/entriesList/EntriesList.js
+++ src/components/entriesList/EntriesList.js
@@ -9,99 +9,105 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import {useState} from "react"
 
+
+// new Intl.DateTimeFormat("en-GB", {
+//   year: "numeric",
+//   month: "long",
+//   day: "2-digit"
+// }).format(entries.entry)
 
 const columns = [
-    { id: 'nume', label: 'Nume', minWidth: 170 },
-    { id: 'prenume', label: 'Prenume', minWidth: 170 },
-    {
-      id: 'data_intrare',
-      label: 'Data intrare',
-      minWidth: 170,
-      align: 'right',
-    },
-    {
-      id: 'companie',
-      label: 'Companie',
-      minWidth: 170,
-      align: 'right',
-    },
-    {
-      id: 'semnatura',
-      label: 'Semnatura',
-      minWidth: 170,
-      align: 'right',
-    },
-  ];
+  { id: 'nume', label: 'Nume', minWidth: 170 },
+  { id: 'prenume', label: 'Prenume', minWidth: 100 },
+  {
+    id: 'data_intrare',
+    label: 'Data intrare',
+    minWidth: 170,
+    align: 'right',
+    format: (Date) => Date.toLocaleDateString(),
+  },
+  {
+    id: 'companie',
+    label: 'Companie',
+    minWidth: 170,
+    align: 'right',
+  },
+  {
+    id: 'semnatura',
+    label: 'Semnatura',
+    minWidth: 170,
+    align: 'right',
   
-  const useStyles = makeStyles({
-    root: {
-      width: '100%',
-    },
-    container: {
-      maxHeight: 440,
-    },
-  });
+  },
+];
 
-  
+const rows = [ ]; 
 
-function EntriesList(){
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
 
-    const classes = useStyles();
-    const [page, setPage] = React.useState(0);
-    const [rows, rowsPerPage, setRowsPerPage] = React.useState(10);
+function EntriesList() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [entries, setEntries] = useState([])
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-      };
-    
-      const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-      };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    return (
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-     <Paper className={classes.root}>
+  return (
+    <Paper className={classes.root}>
        <Header/>
       <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
+        <Table entriesList aria-label="Entries List">
           <TableHead>
             <TableRow>
               {columns.map((column) => (
                 <TableCell
                   key={column.id}
                   align={column.align}
-                  style={{ minWidth: column.ReactDOM.render(
-                    <React.StrictMode>
-                      <UserView/>
-                      {/*<FormAdmin/>*/}
-                      <Root/>
-                    </React.StrictMode>,
-                    document.getElementById('root')
-                  );minWidth }}
-                >
+                  style={{ minWidth: column.minWidth }}>
+                    
                   {column.label}
                 </TableCell>
               ))}
-             </TableRow>
-            </TableHead>
-            <TableBody>                   
-            </TableBody>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+              {entries.map((entries, index) => 
+                <TableRow>
+                        <TableCell>{index + 1}</TableCell>
+                        <TableCell>{entries.title}</TableCell>
+                </TableRow>)}       
+          </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
+        rowsPerPageOptions={[10, 25, 50]}
         component="div"
         count={rows.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onChangePage={handleChangePage}
         onChangeRowsPerPage={handleChangeRowsPerPage}
-      /> 
+      />
     </Paper>
-       
-    )
+  );
 }
 
-export default EntriesList
+export default EntriesList;
