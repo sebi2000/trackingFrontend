acdc5e1cb16922052280e2e62057aa13d316268d
Sebi <stoicescusebastian@yahoo.com>
2021-04-10 00:25:02 +0300

    chore : solve bugs

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,5 @@
 const CONSTANTS = {
-  BACKEND_URL : 'http://localhost/api',
+  BACKEND_URL : 'http://localhost:9000',
   FRONTEND_URL : 'http://localhost:3000',
   LOGIN : 'Log In',
   REGISTER : 'Register',
