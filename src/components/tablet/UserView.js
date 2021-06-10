696c5a6ab0dfdb687703f3de7a6697e4a355d2ac
Sebi <stoicescusebastian@yahoo.com>
2021-06-10 16:22:10 +0300

    feat : add fields to entries

--- src/components/tablet/UserView.js
+++ src/components/tablet/UserView.js
@@ -68,10 +68,17 @@ const styles = theme => ({
     WebkitTransform: 'translate(-50%, -50%)',
     width: '95%'
   },
+  identityFields: {
+    width: '10%',
+    marginRight: '2%'
+  }
 })
 
 
 class UserView extends React.Component{
+
+  durationArray=['00:30 ore', '01:00 ore', '01:30 ore', '02:00 ore', '02:30 ore', '03:00 ore', '03:30 ore', '04.00 ore', 
+                '04:30 ore', '05:00 ore', '05:30 ore', '06:00 ore', '06:30 ore', '07:00 ore', '07:30 ore', '08:00 ore']
 
   state ={
     name : "",
@@ -80,13 +87,21 @@ class UserView extends React.Component{
     phone : "",
     company : "",
     show: false,
+    series: "",
+    number: "",
+    duration: "",
+    observations: "",
     fields: {
       errorName: false,
       errorSurname: false,
       errorEmail: false,
       errorCompany: false,
       errorPhone: false,
-      errorSignature: false
+      errorSignature: false,
+      errorSeries: false,
+      errorNumber: false,
+      errorDuration: false,
+      errorObservations: false,
     },
     companies: []
   }
@@ -122,7 +137,10 @@ class UserView extends React.Component{
       errorEmail: false,
       errorCompany: false,
       errorPhone: false,
-      errorSignature: false
+      errorSignature: false,
+      errorSeries: false,
+      errorNumber: false,
+      errorDuraion: false
     }
 
     Object.entries(this.state.fields).forEach(([key, value]) =>{
@@ -163,6 +181,24 @@ class UserView extends React.Component{
               auxFields.errorSignature= true
             }
         break
+        case 'errorSeries':
+          if(!validator.isAlpha(this.state.series) || !(this.state.series.length >0 && this.state.series.length <3)){
+              isOk=false
+              auxFields.errorSeries= true
+            }
+        break
+        case 'errorNumber':
+          if(!validator.isNumeric(this.state.number) || this.state.number.length !=6){
+              isOk=false
+              auxFields.errorNumber= true
+            }
+        break
+        case 'errorDuration':
+          if(validator.isEmpty(this.state.duration)){
+              isOk=false
+              auxFields.errorDuration= true
+            }
+        break
       }
     })
     this.setState({fields: auxFields})
@@ -176,6 +212,10 @@ class UserView extends React.Component{
         phone : this.state.phone,
         company : this.state.company,
         signature : localStorage.getItem("savedDrawing"),
+        series: this.state.series,
+        number: this.state.number,
+        duration: this.state.duration,
+        observations: this.state.observations,
         date: new Date()
       }
 
@@ -185,6 +225,10 @@ class UserView extends React.Component{
         email : "",
         phone : "",
         company : "",
+        series: "",
+        number: "",
+        duration: "",
+        observations: "",
         show: false
     })
       axios.post("/tablet", {entry}).then(response => {
@@ -213,12 +257,33 @@ class UserView extends React.Component{
              }
             
           <div className={classes.tabletForm}>
-              <div>
-                <TextField error={this.state.fields.errorName} margin="normal" required fullWidth id="name" label={RO.entries.name} name="name" autoComplete="name" autoFocus onChange={ this.onChange }  value={this.state.name}/>
+              
+              <TextField error={this.state.fields.errorName} margin="normal" required fullWidth id="name" label={RO.entries.name} name="name" autoComplete="name" autoFocus onChange={ this.onChange }  value={this.state.name}/>
+              <TextField error={this.state.fields.errorSurname} margin="normal" required fullWidth id="surname" label={RO.entries.surname} name="surname" autoComplete="surname"  onChange={ this.onChange }  value={this.state.surname}/>
+              
+              <div className={classes.flexContainer}>
+                <TextField className={classes.identityFields} error={this.state.fields.errorSeries} margin="normal" required fullWidth id="name" label={RO.entries.series} name="series" autoComplete="name" onChange={ this.onChange }  value={this.state.series}/>
+                <TextField error={this.state.fields.errorNumber} margin="normal" required fullWidth id="name" label={RO.entries.number} name="number" autoComplete="name" onChange={ this.onChange }  value={this.state.number}/>
               </div>
-              <div>
-                <TextField error={this.state.fields.errorSurname} margin="normal" required fullWidth id="surname" label={RO.entries.surname} name="surname" autoComplete="surname"  onChange={ this.onChange }  value={this.state.surname}/>
-              </div>
+              
+              {/* <TextField error={this.state.fields.errorDuration} margin="normal" required fullWidth id="name" label={RO.entries.duration} name="duration" autoComplete="name" onChange={ this.onChange }  value={this.state.duration}/> */}
+              <FormControl fullWidth required margin="normal" error={this.state.fields.errorDuration}>
+                <InputLabel id="demo-simple-select-outlined-label">{RO.entries.duration}</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  value={this.state.duration}
+                  onChange={this.onChange}
+                  name="duration"
+                  >
+              
+                  {this.durationArray.map((time, index) => {
+                      return (<MenuItem value={time}>{time}</MenuItem>)
+                      })
+                  }
+
+                  </Select>
+              </FormControl>
               
               <FormControl fullWidth required margin="normal" error={this.state.fields.errorCompany}>
                 <InputLabel id="demo-simple-select-outlined-label">{RO.entries.company}</InputLabel>
@@ -259,6 +324,9 @@ class UserView extends React.Component{
             
               <TextField className={classes.phone} error={this.state.fields.errorPhone} fullWidth margin="normal" required id="phone" label={RO.entries.phone} name="phone" autoComplete="phone"  onChange={ this.onChange }  value={this.state.phone}/>
             </div>
+
+            <TextField margin="normal" fullWidth id="name" label={RO.entries.observations} name="observations" autoComplete="name" onChange={ this.onChange }  value={this.state.observations}/>
+           
 
         </div>
         <div className={classes.buttonsContainer}>
